ee4a6b3cd9d6422dbde4252d2384ad01ddd1267c
Jesús Ricardo Delgado Sánchez <jesus.delgado5973@alumnos.udg.mx>
2021-10-16 11:40:39 -0500

    Footer, buscar libros en el index, mejoras generales en la base de datos y el index

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -1,3 +1,4 @@
+const {bus} = require("nodemon/lib/utils");
 const controller = {};
 
 controller.index = (req, res) => {
@@ -12,16 +13,24 @@ controller.index = (req, res) => {
 
 controller.search = (req, res) => {
     const data = req.body;
+    const buscar = req.body.search_book;
 
     console.log(data);
 
-    req.getConnection((err, conn) => {
-        conn.query('SELECT * FROM libros WHERE nombre LIKE "%"?"%" OR autor LIKE "%"?"%" OR isbn LIKE "%"?"%" OR editorial LIKE "%"?"%"', [data.search_book, data.search_book, data.search_book, data.search_book], (err, libros) => {
-            res.render('libros_search', {
-                data: libros
+    if (buscar == '') {
+        res.render('libros_search', {
+            data: false
+        });
+    }
+    else {
+        req.getConnection((err, conn) => {
+            conn.query('SELECT * FROM libros WHERE nombre LIKE "%"?"%" OR autor LIKE "%"?"%" OR isbn LIKE "%"?"%" OR editorial LIKE "%"?"%"', [data.search_book, data.search_book, data.search_book, data.search_book], (err, libros) => {
+                res.render('libros_search', {
+                    data: libros
+                });
             });
         });
-    })
+    }
 }
 
 module.exports = controller;
